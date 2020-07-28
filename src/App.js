f9c06f052f68c75f19abd21248368a55effb31b1
Gabriele Jandres <gabrielejandres@gmail.com>
2020-07-27 22:11:58 -0300

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import Menu from './components/Menu'
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div style={{ background: "#141414" }}>
+      <Menu />
+
+      <BannerMain
+        videoTitle={dadosIniciais.categorias[0].videos[0].titulo}
+        url={dadosIniciais.categorias[0].videos[0].url}
+        videoDescription={"Sou desenvolvedora front end, apaixonada pelo mundo de UX & UI, séries e filmes, mas apesar de ser apaixonada por muitas outras coisas, o amor de Jesus é minha maior paixão, o amor dEle é meu Porto Seguro na vida!"}
+      />
+
+      <Carousel
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[1]}
+      />
+
+      <Carousel
+        category={dadosIniciais.categorias[2]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[3]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[4]}
+      />      
+
+      <Carousel
+        category={dadosIniciais.categorias[5]}
+      />      
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FooterBase } from './styles';
+
+function Footer() {
+  return (
+    <FooterBase>
+      <p>
+        Criado por <a href="https://github.com/gabrielejandres"> Gabriele Jandres </a> durante a Imersão React Alura
+      </p>
+    </FooterBase>
+  );
+}
+
+export default Footer;
